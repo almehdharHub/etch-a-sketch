d99aa37337c9c80b3004c1337d7a1a79dd6d51b5
Nawaf almehdhar <181556586+almehdharHub@users.noreply.github.com>
2024-09-26 23:58:08 +0300

    new grid button

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ let container = document.querySelector(".container");
 for (let i = 1; i <= 16; i++) {
   for (let j = 1; j <= 16; j++) {
     let num = document.createElement("p");
-    num.innerHTML = "row " + i.toString() + ", col " + j.toString();
     let div = document.createElement("div");
+    div.style.width = 800 / 16 + "px";
+    div.style.height = 800 / 16 + "px";
     div.appendChild(num);
     container.appendChild(div);
   }
@@ -16,8 +17,33 @@ let divs = document.querySelectorAll(".container div");
 divs.forEach((div) => {
   div.addEventListener("mouseover", () => {
     div.style.backgroundColor = "black";
-    setTimeout(() => {
-      div.style.backgroundColor = "";
-    }, 500);
   });
+});
+
+// new grid button
+let button = document.querySelector("button");
+button.addEventListener("click", () => {
+  let boxs = parseInt(prompt("how many squares per side?"));
+  if (boxs > 100) {
+    alert("too big");
+  } else {
+    container.innerHTML = "";
+    for (let i = 1; i <= boxs; i++) {
+      for (let j = 1; j <= boxs; j++) {
+        let num = document.createElement("p");
+        let div = document.createElement("div");
+        div.style.width = 800 / boxs + "px";
+        div.style.height = 800 / boxs + "px";
+        div.appendChild(num);
+        container.appendChild(div);
+      }
+    }
+    let divs = document.querySelectorAll(".container div");
+
+    divs.forEach((div) => {
+      div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black";
+      });
+    });
+  }
 });
